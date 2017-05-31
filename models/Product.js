22208bfed5ea50173f2bbd0e54d97583f6ad8a2d
oscar luna <os.lun11@gmail.com>
2017-05-30 18:57:54 -0700

    updated

--- models/Product.js
+++ models/Product.js
@@ -32,36 +32,3 @@ var Product = mongoose.model("Product", ProductSchema);
 
 //Export Model
 module.exports = Product;
-
-app.get("/scrape", function(req, res) {
-    //Grab body of html with request
-    request("https://www.dotmed.com/equipment/newest", function(error, response, html){
-        // load page into cheerio 
-        var $ = cheerio.load(html);
-
-        $("div.listings_table_d").each(function(i, element) {
-            var result = {};
-
-            result.name = $(element).children("dt.listing_head").children("a").text().trim() + "";
-            // $(element).find('dt.listing_head h3 a ').text().trim()
-
-            result.link = $(element).children("dt.listing_head").children("a").attr("href").trim();
-
-            result.price = $(element).children("dl.datePosted").children("dd").children("p").text().trim();
-            // $(element).find('div.datePosted p').text().trim()
-
-            var entry = new ProductPage(result);
-            console.log(entry);
-
-            entry.save(function(err, doc) {
-                if (err) {
-                    console.log(err);
-                }
-                else {
-                    console.log(doc);
-                }
-            });
-        });
-    });
-    res.send("Scrape Complere");
-});
